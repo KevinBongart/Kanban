95029fb8173394f40969e020c6da61d0d2a80533
Kevin <contact@kevinbongart.net>
2011-11-23 00:47:29 -0500

    In progress unstackable cards.

--- app/assets/javascripts/stacks.js
+++ app/assets/javascripts/stacks.js
@@ -1,6 +1,15 @@
 $(function() {
   $('.story').click(function() {
-    $(this).parent().toggleClass('expanded');
-    $(this).parent().find('.card').reverseOrder();
+    var stack = $(this).parent();
+    var expanded_stack = $('#expanded-stack');
+    var cards = stack.find('.attached-card');
+
+    stack.toggleClass('expanded');
+    cards.prependTo(expanded_stack);
+    cards.reverseOrder();
+    expanded_stack.css('top', stack.position().top + 132);
+    expanded_stack.toggle();
+    cards.offset(stack.offset());
+    cards.animate({ top: 0, left: 0 }, 300);
   });
 });
