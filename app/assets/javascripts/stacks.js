c2310e05a894b13e804e06d503f67c03c2e2e698
Kevin <contact@kevinbongart.net>
2011-11-27 19:02:12 -0500

    Can't see inside stacks anymore.

--- app/assets/javascripts/stacks.js
+++ app/assets/javascripts/stacks.js
@@ -17,31 +17,31 @@ $(function() {
     $('#inactive-users h3').html(text);
   };
 
-  $('.story').click(function() {
-    var expanded_stack = $('#expanded-stack');
-    var stack = $(this).parent();
-
-    if (stack.hasClass('expanded')) {
-      var cards = expanded_stack.find('.attached-card');
-      if (cards.length > 0) {
-        cards.prependTo(stack);
-        cards.css('position', 'absolute');
-        cards.reverseOrder();
-        expanded_stack.toggle();
-        stack.toggleClass('expanded');
-      }      
-    } else {
-      var cards = stack.find('.attached-card');
-      if (cards.length > 0) {
-        cards.prependTo(expanded_stack);
-        cards.css('position', 'relative');
-        cards.reverseOrder();
-        expanded_stack.css('top', stack.position().top + 114);
-        expanded_stack.toggle();
-        stack.toggleClass('expanded');
-      }
-    }
-  });
+  // $('.story').click(function() {
+  //   var expanded_stack = $('#expanded-stack');
+  //   var stack = $(this).parent();
+  // 
+  //   if (stack.hasClass('expanded')) {
+  //     var cards = expanded_stack.find('.attached-card');
+  //     if (cards.length > 0) {
+  //       cards.prependTo(stack);
+  //       cards.css('position', 'absolute');
+  //       cards.reverseOrder();
+  //       expanded_stack.toggle();
+  //       stack.toggleClass('expanded');
+  //     }      
+  //   } else {
+  //     var cards = stack.find('.attached-card');
+  //     if (cards.length > 0) {
+  //       cards.prependTo(expanded_stack);
+  //       cards.css('position', 'relative');
+  //       cards.reverseOrder();
+  //       expanded_stack.css('top', stack.position().top + 114);
+  //       expanded_stack.toggle();
+  //       stack.toggleClass('expanded');
+  //     }
+  //   }
+  // });
 
   $('.state').sortable({
     connectWith:['.state'],
