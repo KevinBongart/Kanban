d95d4f688aeda4e8e8259de511c849162fd064e8
Kevin <contact@kevinbongart.net>
2011-11-28 19:02:33 -0500

    Style for quickly adding a story.

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,5 +1,12 @@
 $(function() {
   $('#new-story-button').click(function(){
     $('#new-story').toggle();
+    $('#story_name').focus();
+    var link = $(this).children('a');
+    if (link.html() == '+') {
+      link.html('-');
+    } else {
+      link.html('+');
+    }
   });
 });
