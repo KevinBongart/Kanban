a32b971a766cb3e3469539f2fae4c83cdb05b637
Kevin Bongart <contact@kevinbongart.net>
2014-02-25 18:40:52 -0500

    Zurb Foundation

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 gem 'rails', '4.0.3'
 
 gem 'coffee-rails', '~> 4.0.0'
+gem 'foundation-rails'
 gem 'jbuilder', '~> 1.2'
 gem 'jquery-rails'
 gem 'sass-rails', '~> 4.0.0'
--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,5 +1,6 @@
 class BoardsController < ApplicationController
   def show
     @board = Board.includes(lists: { cards: [:todos, :users] }).first
+    @inactive_users = User.where(card_id: nil)
   end
 end
